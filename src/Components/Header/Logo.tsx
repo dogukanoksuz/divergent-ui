2ff36f9545ff1865f89ac9bbd458192b4eea0867
Doğukan Öksüz <me@dogukan.dev>
2022-07-17 13:09:28 +0300

    Small changes

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import dark from '../../Assets/logo_dark.svg';
+import light from '../../Assets/logo_light.svg';
 
 export interface ILogoProps {
     mode: string
@@ -6,6 +8,6 @@ export interface ILogoProps {
 
 export default function Logo(props: ILogoProps) {
     return (
-        <img className="h-14 w-auto logo" src={`/src/Assets/logo_${props.mode}.svg`} alt="Doğukan Öksüz" />
+        <img className="h-14 w-auto logo" src={props.mode == 'dark' ? dark : light} alt="Doğukan Öksüz" />
     );
 }
--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -3,6 +3,8 @@ import { ISummary } from '../Components/Content/Summary';
 import SummaryList from '../Components/Content/SummaryList';
 import Jumbotron from '../Components/Partials/Jumbotron';
 
+import TestThumbnail from '../Assets/test-thumbnail.jpg';
+
 
 export interface IIndexProps {
 }
@@ -21,42 +23,42 @@ export default class Index extends React.Component {
     }
 
     articles: ISummary[] = [{
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
         date: "20 Nisan 2022",
         author: "Doğukan Öksüz"
     }, {
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
         date: "20 Nisan 2022",
         author: "Doğukan Öksüz"
     }, {
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
         date: "20 Nisan 2022",
         author: "Doğukan Öksüz"
     }, {
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
         date: "20 Nisan 2022",
         author: "Doğukan Öksüz"
     }, {
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
         date: "20 Nisan 2022",
         author: "Doğukan Öksüz"
     }, {
-        image: "/src/Assets/test-thumbnail.jpg",
+        image: TestThumbnail,
         title: "Golang Fiber ile Integration Test Geliştirmek",
         url: "#",
         summary: "Bu makalemizde Golang üzerindeki Gofiber frameworkü üzerinde nasıl integration test geliştirebiliriz bu konu üzerine konuşacağız.Integration test nedir buna değinerek başlayabiliriz. Integration test nedir ? Integration test unitlerin birleştiğinde isten...",
